fd2bab13aeaed281c1fa7c319a794a2e8c0341e1
SherazHaider907 <Sherazhaider665@gmail.com>
2026-06-11 14:45:24 +0500

    templates in flask

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
-from flask import Flask, request, redirect, url_for, session, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.secret_key = 'secret-key'
-
 
 @app.route('/')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    username = request.form['username']
-    password = request.form['password']
-
-    if username == 'admin' and password == '123':
-        session['username'] = username
-        return redirect(url_for('welcome'))
-
-    return ('Invalid credentials', 401)
-
-
-@app.route('/welcome')
-def welcome():
-    username = session.get('username')
-    if not username:
-        return redirect(url_for('login'))
-    return render_template('welcome.html', username=username)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def student_profile():
+    return render_template('profile.html',
+    name='John Doe', 
+    is_Topper=True, 
+    major='Computer Science',
+    subjects=['Math', 'Physics', 'Chemistry']
+    )
