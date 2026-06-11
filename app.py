edda7918ae1002dc379b2090de6cfe5fca351461
SherazHaider907 <Sherazhaider665@gmail.com>
2026-06-11 14:21:42 +0500

    simple login

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask , request , redirect , url_for , session , Response
+from flask import Flask, request, redirect, url_for, session, render_template
 
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey" # Set a secret key for session management
+app.secret_key = 'secret-key'
 
-# home page
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    return render_template('login.html')
 
-        if username == 'admin' and password == '1234':
-            session['username'] = username # Store the username in the session
-            return redirect(url_for('welcome'))
-        else:
-            return Response('Invalid credentials', status=401, mimetype='text/plain')
-    return '''
-        <form method="post">
-            <input type="text" name="username" placeholder="Username" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
-            <input type="submit" value="Login">
-        </form>
-    '''
 
-# welcome page
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == 'admin' and password == '123':
+        session['username'] = username
+        return redirect(url_for('welcome'))
+
+    return ('Invalid credentials', 401)
+
+
 @app.route('/welcome')
 def welcome():
-    if 'username' in session:
-        return f''' 
-            <h1>Welcome, {session['username']}!</h1>
-            <a href={url_for('logout')}>Logout</a>
-        '''
-    return redirect(url_for('login'))
+    username = session.get('username')
+    if not username:
+        return redirect(url_for('login'))
+    return render_template('welcome.html', username=username)
 
-# logout page
-@app.route('/logout')
-def logout():
-    session.pop('username', None) # Remove the username from the session
-    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
